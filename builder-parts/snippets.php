d7c4883aa68f6a3f6ac82a8478811eb20ae08e5c
Jon Martin <jon@bigspring.co.uk>
2016-09-06 16:02:04 +0100

    #1 Adds basic support for the builder
    Signed-off-by: Jon Martin <jon@bigspring.co.uk>

---
+++ builder-parts/snippets.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The snippets organism used to render the snippet molecule in loops and snippets shortcodes
+ *
+ * @package monolith
+ */
+?>
+
+<?php if ( $loop->have_posts() ) :  // if we have results run the loop ?>
+
+    <!-- start snippets loop -->
+    <div class="monolith-snippets row <?= $args['classes'] ?>">
+      <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+        <?php if ( is_search() ) : ?>
+          <?php include( $layouts_path . 'molecules/search-result.php' ); ?>
+        <?php else : ?>
+
+
+          <?php include( $part ); ?>
+        <?php endif; ?>
+      <?php endwhile; ?>
+    </div>
+    <!-- end snippets loop -->  
+
+<?php else  :// otherwise show the content none organism ?>
+  <?php include( $layouts_path . 'organisms/content-none.php' ); ?>
+<?php endif; ?>
+
+<?php include( $layouts_path . 'organisms/pagination.php' ); ?>
