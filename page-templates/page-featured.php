55cb28bfd30d18ae758d86c86c5f44d598dc4e18
Harry <harry@bigspring.co.uk>
2016-09-09 12:10:22 +0100

    #2 created page templates

--- page-templates/page-featured.php
+++ page-templates/page-featured.php
@@ -1,27 +1,16 @@
 <?php
-/**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package FoundationPress
- * @since FoundationPress 1.0.0
- */
+/*
+Template Name: Featured Image
+*/
+get_header(); ?>
 
- get_header(); ?>
-
-<?php get_template_part( 'template-parts/header-banner' ); ?>
+<?php get_template_part( 'template-parts/featured-image' ); ?>
 
 	<div id="page-full-width" role="main">
 
 		<?php do_action( 'foundationpress_before_content' ); ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 			<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-<!--				<header>-->
-<!--					<h1 class="entry-title">--><?php //the_title(); ?><!--</h1>-->
-<!--				</header>-->
 				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
 				<div class="entry-content">
 					<?php the_content(); ?>
