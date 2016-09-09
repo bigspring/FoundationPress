839b498026df8c14f8500e78a1eec2bb7f52c544
Julio Taylor <juliotaylor@julio.local>
2016-09-09 11:40:01 +0100

    #1 styled card layout

--- template-parts/card.php
+++ template-parts/card.php
@@ -1,3 +1,33 @@
-<div class="panel">
-Here is a lovely sample card
-</div>
+
+<!-- <div class="card">
+  <a href="<?= get_permalink(); ?>">
+    <?php the_post_thumbnail( 'fp-medium' ); ?>
+    <h3><?php the_title(); ?></h3>
+    <p><?= get_the_excerpt(); ?></p>
+  </a>
+</div> -->
+
+
+<a id="<?= sanitize_title( get_the_title() ) ?>" href="<?php the_permalink(); ?>">
+
+  <div class="m3-card" data-equalizer-watch>
+    <?php if ( $args['has_image'] ) : // display the featured image if argument is true ?>
+      <div class="featured-image">
+        <?php the_post_thumbnail( 'fp-medium' ); ?>
+      </div>
+    <?php endif; // end has_image ?>
+
+    <div class="caption">
+      <?php if ( $args['has_title'] ) : // display only if the summary is enabled (default is true) ?>
+      <h3 class="title <?= $args['has_summary'] ? 'has-summary' : '' ?>"><?php the_title(); ?></h3>
+      <?php endif; // end has_title ?>
+
+      <?php if ( $args['has_summary'] ) : // display only if the summary is enabled (default is true) ?>
+        <div class="summary">
+          <?= wp_trim_words( get_the_excerpt(), 20, '...' ); ?>
+        </div>
+      <?php endif; // end has_summary ?>
+    </div>
+  </div>
+
+</a>
