9e55190df98e31041a3b1c05375252f5eb0c1605
dmseaton <dave@bigspring.co.uk>
2016-10-07 11:08:00 +0100

    Tidy templates

--- page-templates/page-banner.php
+++ page-templates/page-banner.php
@@ -12,6 +12,7 @@ get_header(); ?>
 	<div id="page-full-width" role="main">
 
 		<?php do_action( 'foundationpress_before_content' ); ?>
+		
 		<?php while ( have_posts() ) : the_post(); ?>
 			<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
 				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
