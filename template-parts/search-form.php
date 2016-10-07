c1d5d2a570f0fa525173397e624f971895c04b8e
dmseaton <dave@bigspring.co.uk>
2016-10-07 17:00:04 +0100

    Fixes #27 Add search form part

---
+++ template-parts/search-form.php
@@ -0,0 +1,10 @@
+<form action="<?= site_url(); ?>" id="search-form" method="get">
+	<div class="row collapse">
+		<div class="small-10 columns">
+			<input value="<?php the_search_query(); ?>" type="search" id="s" name="s" placeholder="<?php _e( 'Search the site...', 'monolith' ); ?>">
+		</div>
+		<div class="small-2 columns">
+			<button class="button postfix"><?php _e( 'Go', 'monolith' ); ?></button>
+		</div>
+	</div>
+</form>
