5a5adc9d7d16da8a1c32e5cd4d3aa9a81c963510
simonpmiles <simon@bigspring.co.uk>
2016-10-07 11:24:53 +0100

    Moved monolith option files to new folder

--- page-templates/page-fullwidth.php
+++ page-templates/page-fullwidth.php
@@ -17,10 +17,28 @@ get_header(); ?>
 				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
 				<div class="entry-content">
 					<?php the_content(); ?>
+
+
+<?php monolith_grid('card', 'row small-up-1 medium-up-2 large-up-4',
+  array(
+    'has_image'    => true,
+    'has_summary'  => false,
+    'has_readmore' => false,
+  ),
+  array(
+      'post_type'      => 'page',
+      'posts_per_page' => - 1,
+    )
+  );
+?>
+
+
+
 				</div>
 			</article>
 		<?php endwhile;?>
 		<?php do_action( 'foundationpress_after_content' ); ?>
 	</div>
+
 
 <?php get_footer();
