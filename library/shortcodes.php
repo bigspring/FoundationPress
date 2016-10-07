b8f7a59baf68dfb9b46efbe6d43ee1f39e26ff84
simonpmiles <simon@bigspring.co.uk>
2016-10-07 15:52:45 +0100

    fix to address shortcode

--- library/shortcodes.php
+++ library/shortcodes.php
@@ -30,8 +30,8 @@ function callout_shortcode( $atts, $content = null ) {
 	extract( shortcode_atts( array(
 		'type' => ''
 	), $atts ) );
-	
-	
+
+
 	return '<div class="shortcode-callout callout ' . $type . '">' . apply_filters( 'the_content', $content ) . '</div>';
 }
 
@@ -53,23 +53,23 @@ function buttons( $atts, $content = null ) {
 		'url'    => '',
 		'text'   => '',
 	), $atts ) );
-	
+
 	$type = $type;
-	
+
 	if ( $size == "" ) {
 		$size = "";
 	} else {
 		$size = $size;
 	}
-	
+
 	if ( $pageid != '' ) {
 		$url = get_permalink( $pageid );
 	}
-	
+
 	$output = '<a href="' . $url . '" class="button ' . $type . ' ' . $size . '">';
 	$output .= $text;
 	$output .= '</a>';
-	
+
 	return $output;
 }
 
@@ -87,16 +87,16 @@ function blockquotes( $atts, $content = null ) {
 	extract( shortcode_atts( array(
 		'cite' => '', /* text for cite */
 	), $atts ) );
-	
+
 	$output = '<blockquote>';
 	$output .= '<p>' . $content . '</p>';
-	
+
 	if ( $cite ) {
 		$output .= '<cite>' . $cite . '</cite>';
 	}
-	
+
 	$output .= '</blockquote>';
-	
+
 	return apply_filters( 'the_content', $output );
 }
 
@@ -127,7 +127,7 @@ function childpages( $atts, $content = null ) {
 		'orderby'       => 'menu_order',
 		'order'         => 'ASC'
 	), $atts ) ); // @TODO can we handle these defaults through the builder class instead?
-	
+
 	$args = array(
 		'post_parent'    => $id,
 		'post_type'      => 'page',
@@ -135,7 +135,7 @@ function childpages( $atts, $content = null ) {
 		'orderby'        => $orderby,
 		'posts_per_page' => - 1
 	);
-	
+
 	// define our arguments for the builder based on whether we want to show images, titles, etc
 	$builder_args                 = array();
 	$builder_args['has_image']    = ( $image == 'false' ) ? false : true;
@@ -145,15 +145,15 @@ function childpages( $atts, $content = null ) {
 	$builder_args['has_readmore'] = ( $readmore == 'false' ) ? false : true;
 	$builder_args['classes']      = $class;
 	$builder_args['size']         = $size;
-	
-	
+
+
 	if ( $exclude_pages ) {
 		$args['post__not_in'] = explode( ',', $exclude_pages );
 	}
-	
+
 	ob_start();
 	monolith_build( array( 'layout' => $layout ), $builder_args, $args );
-	
+
 	return ob_get_clean();
 }
 
@@ -162,7 +162,7 @@ add_shortcode( 'childpages', 'childpages' );
 //================================ end childpages shortcode stuff ====================================
 
 function pages_shortcode( $atts, $content = null ) {
-	
+
 	extract( shortcode_atts( array( // set our defaults for the shortcode
 		'layout'        => 'list', // default layout
 		'ids'           => '',
@@ -176,12 +176,12 @@ function pages_shortcode( $atts, $content = null ) {
 		'readmore'      => true,
 		'orderby'       => 'menu_order',
 		'order'         => 'ASC'
-	
+
 	), $atts ) );
-	
+
 	$page_ids = array();
 	$page_ids = explode( ',', $atts['ids'] );
-	
+
 	// get the posts
 	$args = array(
 		'post__in'       => $page_ids,
@@ -189,9 +189,9 @@ function pages_shortcode( $atts, $content = null ) {
 		'order'          => 'ASC',
 		'orderby'        => 'menu_order',
 		'posts_per_page' => - 1
-	
+
 	);
-	
+
 	// define our arguments for the builder based on whether we want to show images, titles, etc
 	$builder_args                 = array();
 	$builder_args['is_thumbnail'] = ( $image_border == 'false' ) ? false : true;
@@ -202,12 +202,12 @@ function pages_shortcode( $atts, $content = null ) {
 	$builder_args['orderby']      = $orderby;
 	$builder_args['classes']      = $class;
 	$builder_args['size']         = $size;
-	
+
 	ob_start();
 	monolith_build( array( $layout ), $builder_args, $args );
-	
+
 	return ob_get_clean();
-	
+
 }//end function
 add_shortcode( 'pages', 'pages_shortcode' );
 
@@ -225,12 +225,12 @@ function list_shortcode( $atts, $content = null ) {
 	extract( shortcode_atts( array(
 		'type' => '', /* no-bullet, ticks, chevron etc */
 	), $atts ) );
-	
-	
+
+
 	$output = '<div class="styled-list ' . $type . '">';
 	$output .= apply_filters( 'the_content', $content );
 	$output .= '</div>';
-	
+
 	return $output;
 }
 
@@ -245,36 +245,36 @@ add_shortcode( 'list', 'list_shortcode' );
  * @return string
  */
 function monolith_foundation_accordion_shortcode( $atts, $content ) {
-	
+
 	extract( shortcode_atts( array( // set our defaults for the shortcode
 		'class' => ''
 	), $atts ) );
-	
+
 	$output = '<dl class="accordion ' . $class . '" data-accordion role="tablist">';
 	$output .= do_shortcode( $content );
 	$output .= '</dl>';
-	
+
 	return apply_filters( 'the_content', $output );
 }
 
 add_shortcode( 'accordion', 'monolith_foundation_accordion_shortcode' );
 
 function monolith_accordion_panel_shortcode( $atts, $content ) {
-	
+
 	extract( shortcode_atts( array( // set our defaults for the shortcode
 		'title' => 'Please enter an accordion title',
 		'class' => ''
 	), $atts ) );
-	
+
 	$id = rand( 1, 1000 );
-	
+
 	$output = '<dd class="accordion-navigation ' . $class . '">';
 	$output .= '<a href="#panel' . $id . '" role="tab" id="panel' . $id . '-heading" aria-controls="panel' . $id . '">' . $title . '</a>';
 	$output .= '<div id="panel' . $id . '" class="content" role="tabpanel" aria-labelledby="panel' . $id . '-heading">';
 	$output .= $content;
 	$output .= '</div>';
 	$output .= '</dd>';
-	
+
 	return apply_filters( 'accordion_panel', $output );
 }
 
@@ -306,11 +306,11 @@ function foundation_columns_shortcode( $atts, $content = null ) {
 	extract( shortcode_atts( array(
 		'columns' => '', /* large-12 small-5 etc */
 	), $atts ) );
-	
+
 	$output = '<div class="columns ' . $columns . '">';
 	$output .= apply_filters( 'the_content', $content );
 	$output .= '</div>';
-	
+
 	return $output;
 }
 
@@ -327,12 +327,12 @@ add_shortcode( 'foundation_columns', 'foundation_columns_shortcode' );
 
 function address_shortcode( $atts, $content = null ) {
 	extract( shortcode_atts( array(
-		'type' => 'no-bullet', /* no-bullet inline-list */
+		'type' => 'horizontal', /* no-bullet inline-list */
 	), $atts ) );
-	
+
 	ob_start();
 	include( get_template_directory() . '/builder-parts/address.php' );
-	
+
 	return ob_get_clean();
 }
 
@@ -346,7 +346,7 @@ function stretch_band_shortcode( $atts, $content = null ) {
 	extract( shortcode_atts( array(
 		'type' => ''
 	), $atts ) );
-	
+
 	return '<div class="m3-shortcode stretch-band ' . $type . '"><div class="row"><div class="columns">' . apply_filters( 'the_content', $content ) . '</div></div></div>';
 }
 
