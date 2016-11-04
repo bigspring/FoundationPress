1da6448968928bebf0ee129eb4df915af494a050
simonpmiles <simon@bigspring.co.uk>
2016-11-04 11:14:29 +0000

    remove junk files

--- builder-parts/molecules/address.php
+++
@@ -1,9 +0,0 @@
-<?php
-$address['details'] = isset( $address['details'] ) ? $address['details'] : get_address();
-$address['class']   = isset( $address['class'] ) ? $address['class'] : '';
-?>
-<ul class="no-bullet address-list <?= $address['class'] ?>">
-  <?php foreach ( $address['details'] as $detail ) : ?>
-    <li><?= $detail ?></li>
-  <?php endforeach; ?>
-</ul>
