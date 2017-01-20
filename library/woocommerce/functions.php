5c41b1ddf4e810ee974297bd69bf45c40f1e8a00
Oliver Fokerd <oliver@bigspring.co.uk>
2017-01-20 16:31:32 +0000

    #28 Add woocommerce support

---
+++ library/woocommerce/functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Date: 20/01/2017
+ * Time: 15:15
+ *
+ * Add more files/includes as necessary, ensuring they exist in the "[theme]/library/woocommerce"
+ */
+
+include (get_template_directory() . '/library/woocommerce/hooks.php');
