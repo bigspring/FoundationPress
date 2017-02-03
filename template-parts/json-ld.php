40bf804daa17afe86f8d0725ea86b95817f52425
Oliver Fokerd <oliver@bigspring.co.uk>
2017-02-03 16:25:01 +0000

    MON-6 Add more schema details

--- template-parts/json-ld.php
+++ template-parts/json-ld.php
@@ -4,44 +4,50 @@ function get_post_data() {
 
 	return $post;
 } // stuff for any page
-$payload["@context"] = "http://schema.org/"; // this has all the data of the post/page etc
-$post_data           = get_post_data(); // stuff for any page, if it exists
-$category            = get_the_category(); // stuff for specific pages
+$payload["@context"] = "http://schema.org/";
+$post_data           = get_post_data();
+$category            = get_the_category();
+$bloginfo            = get_bloginfo();
 if ( is_single() ) { // this gets the data for the user who wrote that particular item
-	$logo                     = get_template_directory_uri() . "/assets/images/logo.png";
-	$author_data              = get_userdata( $post_data->post_author );
-	$post_url                 = get_permalink();
-	$post_thumb               = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
-	$payload["@type"]         = "BlogPosting";
-	$payload["url"]           = $post_url;
-	$payload["author"]        = array( "@type" => "Person", "name" => $author_data->display_name, );
-	$payload["headline"]      = $post_data->post_title;
-	$payload["datePublished"] = $post_data->post_date;
-	$payload["dateModified"]  = $post_data->post_modified;
-//	$payload["image"]         = $post_thumb;
-	$payload["image"]     = array(
-		"@type"  => "ImageObject",
-		"url"    => $post_thumb,
-		"width"  => "100",
-		"height" => "100"
+	$logo                        = get_template_directory_uri() . "/assets/images/logo.png";
+	$author_data                 = get_userdata( $post_data->post_author );
+	$post_url                    = get_permalink();
+	$post_thumb                  = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
+	$payload["@type"]            = "BlogPosting";
+	$payload["url"]              = $post_url;
+	$payload["author"]           = array(
+		"@type" => "Person",
+		"name"  => $author_data->display_name
 	);
-	$payload["genre"]     = $category[0]->cat_name;
-	$payload["publisher"] = array(
+	$payload["headline"]         = $post_data->post_title;
+	$payload["datePublished"]    = $post_data->post_date;
+	$payload["dateModified"]     = $post_data->post_modified;
+	$payload["image"]            = $post_thumb;
+	$payload["genre"]            = $category[0]->cat_name;
+	$payload["publisher"]        = array(
 		"@type" => "Organization",
 		"name"  => "Hallam Internet Ltd",
 		"logo"  => $logo
 	);
+	$payload["mainEntityOfPage"] = array(
+		"@type" => "WebPage",
+		"@id"   => "http://cathscafe.example.com/"
+	);
 } // we do all this separately so we keep the right things for organization together
 if ( is_front_page() ) {
+	$post_url                = get_permalink();
 	$payload["@type"]        = "Organization";
 	$payload["name"]         = "Builtvisible";
 	$payload["logo"]         = "http://builtvisible.com/wp-content/uploads/2014/05/BUILTVISIBLE-Badge-Logo-512x602-medium.png";
-	$payload["url"]          = "http://builtvisible.com/";
+	$payload["url"]          = $post_url;
 	$payload["sameAs"]       = array(
-		"https://twitter.com/builtvisible",
-		"https://www.facebook.com/builtvisible",
-		"https://www.linkedin.com/company/builtvisible",
-		"https://plus.google.com/+SEOgadget/"
+		get_option( 'monolith_twitter' ),
+		get_option( 'monolith_facebook' ),
+		get_option( 'monolith_linkedin' ),
+		get_option( 'monolith_googleplus' ),
+		get_option( 'monolith_youtube' ),
+		get_option( 'monolith_pinterest' ),
+		get_option( 'monolith_instagram' )
 	);
 	$payload["contactPoint"] = array(
 		array(
@@ -53,25 +59,17 @@ if ( is_front_page() ) {
 	);
 }
 if ( is_author() ) { // this gets the data for the user who wrote that particular item
-	$author_data = get_userdata( $post_data->post_author );
-// some of you may not have all of these data points in your user profiles - delete as appropriate // fetch twitter from author meta and concatenate with full twitter URL
-	$twitter_url       = " https://twitter.com/";
-	$twitterHandle     = get_the_author_meta( 'twitter' );
-	$twitterHandleURL  = $twitter_url . $twitterHandle;
-	$websiteHandle     = get_the_author_meta( 'url' );
-	$facebookHandle    = get_the_author_meta( 'facebook' );
-	$gplusHandle       = get_the_author_meta( 'googleplus' );
-	$linkedinHandle    = get_the_author_meta( 'linkedin' );
-	$slideshareHandle  = get_the_author_meta( 'slideshare' );
+	$author_data       = get_userdata( $post_data->post_author );
 	$payload["@type"]  = "Person";
 	$payload["name"]   = $author_data->display_name;
 	$payload["email"]  = $author_data->user_email;
 	$payload["sameAs"] = array(
-		$twitterHandleURL,
-		$websiteHandle,
-		$facebookHandle,
-		$gplusHandle,
-		$linkedinHandle,
-		$slideshareHandle
+		get_option( 'monolith_twitter' ),
+		get_option( 'monolith_facebook' ),
+		get_option( 'monolith_linkedin' ),
+		get_option( 'monolith_googleplus' ),
+		get_option( 'monolith_youtube' ),
+		get_option( 'monolith_pinterest' ),
+		get_option( 'monolith_instagram' )
 	);
 }
