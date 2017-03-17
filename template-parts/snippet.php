14064411c8749c1de00303f1285298fe40fc6177
Harry <harry@bigspring.co.uk>
2017-03-17 10:21:38 +0000

    MON-6 Remove microdate from card and snippets

--- template-parts/snippet.php
+++ template-parts/snippet.php
@@ -7,19 +7,18 @@
  * @package FoundationPress
  * @since FoundationPress 1.0.0
  */
-
 ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class('snippet'); ?>>
-	<header>
-		<h2><a href="<?php the_permalink(); ?>" itemprop="url"><?php the_title(); ?></a></h2>
+		<header>
+				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<?php // foundationpress_entry_meta(); ?>
-	</header>
-	<div class="entry-content">
-		<p><?= wp_trim_words( get_the_excerpt(), 30, '...' ); ?></p>
-	</div>
-	<footer>
-		<a href="<?php the_permalink(); ?>" class="read-more button"><?php _e( 'Read more', 'foundationpress' ); ?></a>
-	</footer>
-	<hr />
+		</header>
+		<div class="entry-content">
+				<p><?php echo wp_trim_words( get_the_excerpt(), 30, '...' ); ?></p>
+		</div>
+		<footer>
+				<a href="<?php the_permalink(); ?>" class="read-more button"><?php _e( 'Read more', 'foundationpress' ); ?></a>
+		</footer>
+		<hr />
 </div>
