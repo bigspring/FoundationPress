ffeb1c936ff1798219ff06d0402e0cca237af925
dmseaton <dave.seaton@hallaminternet.com>
2017-06-20 15:16:48 +0100

    MON-28 Add action hooks to builder parts (inside loop)

--- builder-parts/accordion.php
+++ builder-parts/accordion.php
@@ -4,22 +4,26 @@
  *
  * @package monolith
  */
- ?>
+?>
 
 <?php if ( $loop->have_posts() ) : // if we have a loop load the accordion organism ?>
-
-  <ul class="accordion" data-accordion data-allow-all-closed="true">
-
-    <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-
-      <?php get_template_part('template-parts/accordion-item'); ?>
-
-    <?php endwhile; ?>
-
-    </ul>
+	
+	<?php do_action( 'monolith_before_builder_accordion', $loop ); ?>
+	
+	<ul class="accordion" data-accordion data-allow-all-closed="true">
+		
+		<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+			
+			<?php get_template_part( 'template-parts/accordion-item' ); ?>
+		
+		<?php endwhile; ?>
+	
+	</ul>
+	
+	<?php do_action( 'monolith_after_builder_accordion', $loop ); ?>
 
 <?php else : // otherwise load the content-none molecule ?>
-
-      <?php get_template_part('template-parts/content-none'); ?>
+	
+	<?php get_template_part( 'template-parts/content-none' ); ?>
 
 <?php endif;
--- builder-parts/tabs.php
+++ builder-parts/tabs.php
@@ -12,24 +12,28 @@
 	
 	<?php $count = 0; // initiate the count ?>
 	
+	<?php do_action( 'monolith_before_builder_tabs', $loop ); ?>
+	
 	<!-- the tabs navigation   -->
 	<ul class="tabs" data-tabs id="builder-tabs">
-	  <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-				<li class="tabs-title <?php echo $count === 0 ? 'is-active' : '' ?>"><a
-						href="#panel-<?php echo get_the_id() ?>"><?php the_title(); ?></a></li>
-		  <?php $count ++ ?>
-	  <?php endwhile; ?>
+		<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+			<li class="tabs-title <?php echo $count === 0 ? 'is-active' : '' ?>"><a
+					href="#panel-<?php echo get_the_id() ?>"><?php the_title(); ?></a></li>
+			<?php $count ++ ?>
+		<?php endwhile; ?>
 	</ul>
 	<!-- end tab layout wrapper -->
 	<?php $count = 0; // reset the count  ?>
 	
 	<!-- the tabs content -->
 	<div class="tabs-content" data-tabs-content="builder-tabs">
-	  <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+		<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 			<?php include( $part ); ?>
-		  <?php $count ++ ?>
-	  <?php endwhile; ?>
+			<?php $count ++ ?>
+		<?php endwhile; ?>
 	</div>
+	
+	<?php do_action( 'monolith_after_builder_tabs', $loop ); ?>
 
 <?php else : // otherwise load the content-none molecule ?>
 	<?php get_template_part( 'template-parts/content-none' ); ?>
