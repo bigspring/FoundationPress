a2364679dae63237b5aec08a87f9a3a5490ab06d
Harry <harry@bigspring.co.uk>
2017-11-08 11:03:20 +0000

    Remove duplicate call to browser sync in footer and only use the call in hooks

--- library/monolith/hooks.php
+++ library/monolith/hooks.php
@@ -101,7 +101,7 @@ add_action( 'foundationpress_before_closing_body', function () {
 	}
 	
 	echo "<script id=\"__bs_script__\">//<![CDATA[
-		document.write(\"<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.17.0'><\/script>\".replace(\"HOST\", location.hostname));
+		document.write(\"<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.2'><\/script>\".replace(\"HOST\", location.hostname));
 		//]]></script>";
 }, 99 );
 
