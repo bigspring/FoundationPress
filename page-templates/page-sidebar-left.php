67cf21b06a8d59d96aee271260a8935844a650f8
simonpmiles <simon@bigspring.co.uk>
2017-12-29 10:48:00 +0000

    fixes to left and right sidebar pages

--- page-templates/page-sidebar-left.php
+++ page-templates/page-sidebar-left.php
@@ -6,45 +6,23 @@ Template Name: Left Sidebar
  * @since FoundationPress 1.0.0
 */
 get_header(); ?>
-<?php /*
-	
-	<div class="main-wrap sidebar-left" role="main">
-		
-		<?php do_action( 'foundationpress_before_content' ); ?>
-		
-		<?php while ( have_posts() ) : the_post(); ?>
-			<article <?php post_class( 'main-content' ) ?> id="post-<?php the_ID(); ?>">
-				
-				<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
-				<?php get_template_part( 'template-parts/header-standard' ); ?>
-				
-				
-				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-				
-				<div class="entry-content">
-					<?php the_content(); ?>
-				</div>
-			
-			</article>
-		<?php endwhile; ?>
-		
-		<?php do_action( 'foundationpress_after_content' ); ?>
-		<?php get_sidebar(); ?>
-	
-	</div>
 
-*/ ?>
+<?php get_template_part( 'template-parts/header-standard' ); ?>
+<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
 
-<?php get_template_part( 'template-parts/featured-image' ); ?>
 <div class="main-container">
 	<div class="main-grid sidebar-left">
 		<main class="main-content">
-			<?php while ( have_posts() ) : the_post(); ?>
+			<?php
+			while ( have_posts() ) :
+				the_post();
+?>
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 				<?php comments_template(); ?>
 			<?php endwhile; ?>
-		 </main>
-	<?php get_sidebar(); ?>
+		</main>
+		<?php get_sidebar(); ?>
 	</div>
 </div>
-<?php get_footer();
+<?php
+get_footer();
--- page-templates/page-sidebar-right.php
+++ page-templates/page-sidebar-right.php
@@ -6,27 +6,24 @@ Template Name: Right Sidebar
  * @since FoundationPress 1.0.0
 */
 get_header(); ?>
-	
-	<div class="main-wrap sidebar-right" role="main">
-		
-		<?php do_action( 'foundationpress_before_content' ); ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<article <?php post_class( 'main-content' ) ?> id="post-<?php the_ID(); ?>">
-				
-				<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
-				<?php get_template_part( 'template-parts/header-standard' ); ?>
-				
-				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-				<div class="entry-content">
-					<?php the_content(); ?>
-				</div>
-			
-			</article>
-		<?php endwhile; ?>
-		
-		<?php do_action( 'foundationpress_after_content' ); ?>
-		<?php get_sidebar(); ?>
-	
-	</div>
 
-<?php get_footer();
+
+<?php get_template_part( 'template-parts/header-standard' ); ?>
+<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
+
+<div class="main-container">
+	<div class="main-grid">
+		<main class="main-content">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+?>
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php comments_template(); ?>
+			<?php endwhile; ?>
+		</main>
+		<?php get_sidebar(); ?>
+	</div>
+</div>
+<?php
+get_footer();
