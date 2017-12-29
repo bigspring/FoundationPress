21bdf6a600c98316470667535adcc7d873c28f5f
simonpmiles <simon@bigspring.co.uk>
2017-12-29 11:36:03 +0000

    fixes wrapping markup on 404.php

--- page-templates/sitemap.php
+++ page-templates/sitemap.php
@@ -7,26 +7,17 @@ Template Name: Sitemap
 */
 get_header(); ?>
 
-<?php get_template_part( 'template-parts/header-banner' ); ?>
-	
-	<div class="main-wrap full-width" role="main">
-		
-		<?php do_action( 'foundationpress_before_content' ); ?>
-		
-		<?php while ( have_posts() ) : the_post(); ?>
-			<article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-				<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-				<div class="entry-content">
-					<?php the_content(); ?>
-					
-					<h1><?php the_title(); ?></h1>
-					<ul class="no-bullet"><?php wp_list_pages("title_li=" ); ?></ul>
-				</div>
-			</article>
-		<?php endwhile;?>
-		
-		<?php do_action( 'foundationpress_after_content' ); ?>
-	
-	</div>
+<?php get_template_part( 'template-parts/header-standard' ); ?>
+<?php get_template_part( 'template-parts/breadcrumbs' ); ?>
 
+<div class="main-container">
+	<div class="main-grid">
+		<main class="main-content-full-width">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<ul class="no-bullet"><?php wp_list_pages("title_li=" ); ?></ul>
+			<?php endwhile; ?>
+		</main>
+	</div>
+</div>
 <?php get_footer();
