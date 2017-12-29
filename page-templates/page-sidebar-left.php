53537aed38fea7e8020a9f1e7ab3d2d581474b0a
simonpmiles <simon@bigspring.co.uk>
2017-12-29 10:51:30 +0000

    tidies while loop markup

--- page-templates/page-sidebar-left.php
+++ page-templates/page-sidebar-left.php
@@ -13,10 +13,7 @@ get_header(); ?>
 <div class="main-container">
 	<div class="main-grid sidebar-left">
 		<main class="main-content">
-			<?php
-			while ( have_posts() ) :
-				the_post();
-?>
+			<?php while ( have_posts() ) : the_post(); ?>
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 				<?php comments_template(); ?>
 			<?php endwhile; ?>
